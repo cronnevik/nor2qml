8a4f00ff1920f2b39078d3c651213fed09bada39
Christian Rønnevik <cronnevik@gmail.com>
2021-11-09 09:28:18 +0100

    removed unnecessary build scripts as the applications are build by profiles

--- devops/deploy-web-service-on-local.sh
+++
@@ -1,8 +0,0 @@
-cd ../seisan-quakeml-commons-jpa
-mvn clean install
-cd ../converter-core
-mvn clean install
-cd ../seisan-quakeml-commons-web
-mvn clean install
-cd ../quakeml-web-service
-mvn clean install
