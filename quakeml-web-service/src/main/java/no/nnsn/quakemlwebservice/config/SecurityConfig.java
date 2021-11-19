53b2338f1b9edd601b4eda3eee3f0fa802cbc7f6
Christian Rønnevik <cronnevik@gmail.com>
2021-11-19 13:26:45 +0100

    web security added for actuator endpoints with sensitive application information. fixed bug of listing catalogs within jersey due to issue with media dependency

---
+++ quakeml-web-service/src/main/java/no/nnsn/quakemlwebservice/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package no.nnsn.quakemlwebservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/fdsnws/event/1/**")
+                .antMatchers("/dataform")
+                .antMatchers("/");
+    }
+}
